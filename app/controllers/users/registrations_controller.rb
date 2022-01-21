23aec70bac9e852fd4e0db2de07760328e704615
Kapatsila_Mariia <m.kapatsila@gmail.com>
2022-01-21 22:48:14 +0200

    changed migrations and added authentication

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,25 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  def create
+    user = User.create(user_params)
+  end
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :login, :role_id)
+  end
+
+  def respond_with(resource, _opts = {})
+    register_success && return if resource.persisted?
+
+    register_failed
+  end
+
+  def register_success
+    render json: { message: 'Signed up sucessfully.' }
+  end
+
+  def register_failed
+    render json: { message: "Something went wrong." }
+  end
+end
